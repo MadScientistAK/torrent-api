b7cbd626154b6a21eb333f80470810917e1359bb
MadScientistAK <anmolzagrawal@gmail.com>
2020-08-01 13:26:04 +0530

    Made a package to be used in other files.

--- torscraper.go
+++ torscraper.go
@@ -1,7 +1,6 @@
-package main
+package torscraper
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -10,7 +9,8 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-type torrent struct {
+// Torrent is a struct to hold torrent data
+type Torrent struct {
 	tname     string `json:"TorrentName"`
 	tlink     string `json:"MagnetLink"`
 	tseeders  string `json:"Seeders"`
@@ -18,11 +18,12 @@ type torrent struct {
 	tsize     string `json:"Size"`
 }
 
-var base_link string = "https://1337x.to"
+var baseLink string = "https://1337x.to"
 
 var wg sync.WaitGroup
 
-func getMagnet(tlink *string, wg *sync.WaitGroup) {
+// GetMagnet is a function to get magnet links from 1337x.to
+func GetMagnet(tlink *string, wg *sync.WaitGroup) {
 	tlink1 := "https://1337x.to" + *tlink
 	res, _ := http.Get(tlink1)
 	defer res.Body.Close()
@@ -33,16 +34,17 @@ func getMagnet(tlink *string, wg *sync.WaitGroup) {
 	defer wg.Done()
 }
 
-func getTorrents(query string, wg *sync.WaitGroup) ([]torrent, string) {
-	query = base_link + "/search/" + strings.Replace(query, " ", "+", -1) + "/1/"
+// GetTorrents is a function to get torrents from 1337x.to
+func GetTorrents(query string, wg *sync.WaitGroup) ([]Torrent, string) {
+	query = baseLink + "/search/" + strings.Replace(query, " ", "+", -1) + "/1/"
 	res, err := http.Get(query)
 	defer res.Body.Close()
 	if err != nil {
 		log.Fatal(err)
-		return make([]torrent, 0), "No Results Returned"
+		return make([]Torrent, 0), "No Results Returned"
 	}
 
-	torrentList := make([]torrent, 0)
+	torrentList := make([]Torrent, 0)
 
 	doc, _ := goquery.NewDocumentFromReader(res.Body)
 
@@ -55,13 +57,9 @@ func getTorrents(query string, wg *sync.WaitGroup) ([]torrent, string) {
 			torsize := strings.Replace(s.Find(".size").Text(), torseeders, "", -1)
 			torrentList = append(torrentList, torrent{tname: tor.Text(), tlink: torurl, tseeders: torseeders, tleechers: torleechers, tsize: torsize})
 			wg.Add(1)
-			go getMagnet(&torrentList[i-1].tlink, wg)
+			go GetMagnet(&torrentList[i-1].tlink, wg)
 		}
 	})
 	wg.Wait()
 	return torrentList, ""
-}
-
-func main() {
-	fmt.Println(getTorrents("Jump Force", &wg))
 }
