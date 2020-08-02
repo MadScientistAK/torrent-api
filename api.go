f91aedce28083e1a0be31f7ca24d368e158fb967
MadScientistAK <anmolzagrawal@gmail.com>
2020-08-02 13:29:11 +0530

    Added api file

---
+++ api.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/MadScientistAK/torscraper"
+	"github.com/gofiber/fiber"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/searchNyaa/:query?", func(c *fiber.Ctx) {
+		tmp := c.Params("query")
+		if tmp != "" {
+			tmp = strings.ReplaceAll(tmp, "%20", "+")
+			c.Send(torscraper.GetAnimeTorrents(tmp))
+		} else {
+			c.Send("Where is the query?")
+		}
+	})
+
+	app.Get("/search1337/:query?", func(c *fiber.Ctx) {
+		tmp := c.Params("query")
+		if tmp != "" {
+			tmp = strings.ReplaceAll(tmp, "%20", "+")
+			c.Send(torscraper.GetTorrents(tmp))
+		} else {
+			c.Send("Where is the query?")
+		}
+	})
+
+	app.Listen(3000)
+}
